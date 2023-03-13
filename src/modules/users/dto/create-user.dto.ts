f62970413c627c7b27dbca13ffb3d0dd4d36c2c8
truongnx <nguyenxuantruong13112000@gmail.com>
2023-03-13 16:33:38 +0700

    test

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({
+    example: 'user1',
+  })
+  username: string;
+
+  @ApiProperty({
+    example: '123456aA@',
+  })
+  password: string;
+
+  @ApiProperty({
+    example: 'user1@gmail.com',
+  })
+  @IsEmail()
+  email: string;
+}
